5ae948e670e672a7081e79d8efdc33e0cd3735c4
Farzeen <happycoder97@protonmail.com>
2021-02-26 12:47:12 +0530

    Fix: camera moves away when rotating
    This was caused by using small tangential movements to approximate
    rotations. This will inevitably, slowly diverge.
    Fixed by doing actual rotation instead of small movements along tangent.

--- camera.py
+++ camera.py
@@ -36,16 +36,26 @@ class Camera:
         )  # type: ignore
 
     def rotate(self, x, y):
-        amt = 4.0
-
+        x_factor = 90.0
+        y_factor = 90.0
         (cameraPrincipal, cameraRight, cameraUp) = self._getCamVecs()
 
-        delta = amt * (x * cameraRight + y * cameraUp)
+        # x_rot_axis = glm.vec3(0,1,0)
+        x_rot_axis = cameraUp
+        x_angle = glm.radians(-x * x_factor)
+        rot_x = glm.rotate(glm.identity(glm.mat4), x_angle, x_rot_axis)
 
-        self.position += delta
+        y_rot_axis = cameraRight
+        y_angle = glm.radians(y * y_factor)
+        rot_y = glm.rotate(glm.identity(glm.mat4), y_angle, y_rot_axis)
+
+        rot = rot_y * rot_x
+        posDir = glm.vec4(self.position - self.lookAt, 0.0)
+        posDirRotated = rot * posDir
+        self.position = self.lookAt + glm.vec3(posDirRotated)
 
     def move(self, x, y):
-        amt = 2.0
+        amt = 4.0
 
         (cameraPrincipal, cameraRight, cameraUp) = self._getCamVecs()
 
